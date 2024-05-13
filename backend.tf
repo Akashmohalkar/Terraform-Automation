91e696a86438020bd402c9ce3817040e3e7f9b12
Amolgajakosh <165985393+Amolgajakosh@users.noreply.github.com>
2024-05-13 15:29:32 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-amol"
+    bucket = "mydev-tf-state-bucket-project-terraform-unicorn"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
